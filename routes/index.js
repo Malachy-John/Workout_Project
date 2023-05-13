53fe74cda0401e878d4251da9e18929e2a4c5d01
Malachy McInnes <whitemalteser@gmail.com>
2023-05-13 21:47:42 +0100

    Adding server

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 const bodyParser = require('body-parser');
 var cors = require('cors')
 const fs = require('fs')
