9bf39f3c3012f15a7900bcb0abcaab3eb96a287f
Malachy McInnes <whitemalteser@gmail.com>
2023-05-19 14:31:03 +0100

    Great server addition again 8th

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.post('/post-test', (req, res) => {
 
   console.log(req.body)
 
-  fs.writeFile('saved_workouts.json', requested_val);
+  fs.writeFile('/routes/saved_workouts.json', requested_val);
   //fs.writeFile('../saved_workouts.json', requested_val);
 
 });
